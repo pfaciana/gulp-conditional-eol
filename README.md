645759a89618ea39618d2399cf08bb63496ddd22
pfaciana <phil@renderdev.com>
2017-11-20 00:42:54 -0500

    init

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # gulp-conditional-eol
-Gulp Plugin for Line Ending Corrector (A utility that makes sure your files have consistent line endings)
+
+Gulp Plugin for Line Ending Corrector (A utility that makes sure your files have consistent line endings) 
+
+# about gulp-conditional-eol
+
+It converts all those pesky `\r\n` (a.k.a `CRLF`) line endings in Microsoft Windows operating systems into the more commonly used and recognized `\n` (a.k.a `LF`). Though it lets you do the opposite as well ( converting `LF` to `CRLF` ). It supports `\r` (a.k.a `CR`) as well for the sake of completion.
+
+You should definitely have this in your build process especially if someone in your team works from a non UNIX system.
+
+
+# Usage
+
+requiring it. the condEOL variable itself is a function.
+
+```node
+condEOL = require('gulp-conditional-eol')
+```
+
+simplest version
+
+```node
+.pipe(condEOL())
+```
+
+with all options
+
+```node
+.pipe(condEOL({
+	verbose: 'min', 
+	lineSeparator: 'LF', 
+	fileEncoding: 'utf8', 
+	ext: ['scss','css','js']
+}, ['additionalExt1','additionalExt2']))
+```
+
+## First Parameter: Options (object)
+
+`verbose`
+Accepts 'min', true or false. 'min' only shows updated files, true shows all, false hides all. Default 'min'
+
+`lineSeparator`
+Desired End of Line character. can be `CR` (`\r`), `LF`(`\n`). Default `LF`(`\n`)
+
+`encoding`
+Any meaningful encoding that nodejs supports. Default `utf8`
+
+`ext`
+An array of all the file extensions to check (replaces existing defaults). 
+Default [
+'cnf', 'conf', 'config', 'css', 'haml', 'htaccess', 'htm', 'html', 'jade', 'js', 'json', 'log',
+'markdown', 'md', 'mustache', 'php', 'pug', 'scss', 'tpl', 'ts', 'txt', 'xhtml', 'xml', 'yml'
+]
+
+## Second Parameter: appendExt (string or array)
+
+Additional file extensions to add to the defaults. Default [] (empty)
+
+
---
+++ index.js
@@ -0,0 +1,77 @@
+'use strict';
+
+var lec = require('line-ending-corrector').LineEndingCorrector,
+	path = require('path'),
+	through = require('through2'),
+	defaults = {
+		lineSeparator: 'LF',
+		fileEncoding: 'utf8',
+		verbose: 'min',
+		ext: [
+			'cnf', 'conf', 'config', 'css', 'haml', 'htaccess', 'htm', 'html', 'jade', 'js', 'json', 'log',
+			'markdown', 'md', 'mustache', 'php', 'pug', 'scss', 'tpl', 'ts', 'txt', 'xhtml', 'xml', 'yml'
+		]
+	},
+	PLUGIN_NAME = 'ConditionalEOL',
+	CONSOLE_PREFIX = PLUGIN_NAME + ': ';
+
+module.exports = function (opt, appendExt) {
+	opt = opt || {};
+	opt.lineSeparator = opt.lineSeparator || defaults.lineSeparator;
+	opt.fileEncoding = opt.fileEncoding || defaults.fileEncoding;
+	opt.verbose = opt.verbose || defaults.verbose;
+	opt.ext = opt.ext || defaults.ext;
+	appendExt = appendExt || [];
+
+	// Merge default ext and user ext
+	if (!Array.isArray(appendExt)) {
+		appendExt = [appendExt];
+	}
+
+	opt.ext.push.apply(opt.ext, appendExt);
+
+	function stream(file, encoding, callback) {
+		var ext = path.extname(file.path).substring(1),
+			response, wasAltered, output;
+
+		// If null or file extension does not match
+		if (file.isNull() || !opt.ext.includes(ext)) {
+			if (!file.isNull() && typeof opt === 'object' && 'verbose' in opt && opt.verbose === true) {
+				console.log(CONSOLE_PREFIX + file.path + ' - file extension does not match');
+			}
+			return callback(null, file);
+		}
+
+		if (file.isStream()) {
+			return callback(CONSOLE_PREFIX + 'Streaming not supported');
+		}
+
+		try {
+			response = lec.correctSync(file.contents.toString(opt.fileEncoding), {
+				verbose: opt.verbose,
+				eolc: opt.lineSeparator,
+				encoding: opt.fileEncoding
+			});
+			wasAltered = response[0];
+			output = response[1];
+			if (wasAltered) {
+				if (typeof opt === 'object' && 'verbose' in opt && opt.verbose) {
+					console.log(CONSOLE_PREFIX + file.path + ' - CONVERTED TO ' + opt.lineSeparator);
+				}
+				file.contents = new Buffer(output);
+				this.push(file);
+			} else {
+				if (typeof opt === 'object' && 'verbose' in opt && opt.verbose === true) {
+					console.log(CONSOLE_PREFIX + file.path + ' - already is ' + opt.lineSeparator);
+				}
+
+			}
+		} catch (err) {
+			return callback(err);
+		}
+
+		return callback();
+	}
+
+	return through.obj(stream);
+};
